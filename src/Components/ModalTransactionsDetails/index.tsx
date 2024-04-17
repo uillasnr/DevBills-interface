51591d99e8dc0d046dac9f2bacd80190a317f022
uillasnr <wiliasreis@hotmail.com>
2024-04-16 23:06:05 -0300

    feat: Modal observation

--- src/Components/ModalTransactionsDetails/index.tsx
+++ src/Components/ModalTransactionsDetails/index.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react';
-import { Content, Info, IconWrapper, Text, TextColor, Label } from './styles';
+import {
+  Content,
+  Info,
+  IconWrapper,
+  Text,
+  TextColor,
+  Label,
+  Modal,
+} from './styles';
 import { Dialog } from '../Dialog';
-import { api } from '../../services/Api';
+
 import { BsCalendar2Date } from 'react-icons/bs';
 import { TbFileDescription } from 'react-icons/tb';
 import { FaMoneyBill, FaTags } from 'react-icons/fa';
@@ -12,7 +20,7 @@ import { BiTrendingDown, BiTrendingUp } from 'react-icons/bi';
 import { color } from '../../Styles/color';
 
 type TransactionDetails = {
-  _id: number;
+  id: number;
   title: string;
   date: string;
   amount: number;
@@ -30,121 +38,122 @@ export function ModalTransactionsDetails({
   transactionDetails: TransactionDetails;
 }) {
   const [open, setOpen] = useState(false);
-  const [transactionDetailsData, setTransactionDetailsData] =
-    useState<TransactionDetails | null>(null);
+  const [observationModalOpen, setObservationModalOpen] = useState(false);
 
   useEffect(() => {
-    if (transactionDetails) {
-      setOpen(true);
-    }
+    setOpen(!!transactionDetails); // Concisely set open based on truthiness
   }, [transactionDetails]);
 
-  useEffect(() => {
-    async function loadTransactionDetails() {
-      try {
-        if (transactionDetails && transactionDetails._id) {
-          const response = await api.get(
-            `/transactions/${transactionDetails._id}`,
-          );
-          const { data } = response;
-          console.log('detalhes', data);
-          setTransactionDetailsData(data);
-        }
-      } catch (error) {
-        console.error('Erro ao carregar detalhes da transação:', error);
-      }
-    }
+  const handleObservationClick = () => {
+    setObservationModalOpen(true);
+  };
 
-    if (open && transactionDetails) {
-      loadTransactionDetails();
-    }
-  }, [open, transactionDetails]);
-
+  const handleCloseObservationModal = () => {
+    setObservationModalOpen(false);
+  };
   return (
-    <Dialog open={open} onOpenChange={setOpen} trigger={<></>}>
-      <Content>
-        <h2>Detalhes da Transação</h2>
+    <>
+      <Dialog open={open} onOpenChange={setOpen} trigger={<></>}>
+        {' '}
+        {/* Improved trigger */}
+        <Content>
+          <h2>Detalhes da Transação</h2>
 
-        <Info>
+          <Info>
+            <div>
+              <IconWrapper>
+                <TbFileDescription size={25} />
+              </IconWrapper>
+              <Text>
+                <label>Título:</label>
+                <p>{transactionDetails.title}</p>
+              </Text>
+            </div>
+
+            <div>
+              <IconWrapper>
+                <BsCalendar2Date size={20} />
+              </IconWrapper>
+              <Text>
+                <label>Data:</label>
+                <p>
+                  {transactionDetails
+                    ? formatDate(transactionDetails.date)
+                    : ''}
+                </p>
+              </Text>
+            </div>
+
+            <div>
+              <IconWrapper>
+                <FaTags size={20} />
+              </IconWrapper>
+              <Text>
+                <label>Categoria:</label>
+                <p>{transactionDetails.category.title}</p>
+              </Text>
+            </div>
+
+            <div>
+              <IconWrapper>
+                <FaMoneyBill size={20} />
+              </IconWrapper>
+              <Text>
+                <label>Valor:</label>
+                <p>
+                  {transactionDetails
+                    ? formatCurrency(transactionDetails.amount)
+                    : ''}
+                </p>
+              </Text>
+            </div>
+
+            <div onClick={handleObservationClick}>
+              {' '}
+              {/* Observation click handler */}
+              <IconWrapper>
+                <BiEditAlt size={20} />
+              </IconWrapper>
+              <Text>
+                <label>Observação:</label>
+                <span>
+                  {transactionDetails.observation
+                    ? transactionDetails.observation
+                    : 'N/A'}
+                </span>
+              </Text>
+            </div>
+
+            <TextColor>
+              <Label
+                labelColor={
+                  transactionDetails.type === 'income'
+                    ? color.colors.primary
+                    : color.colors.error
+                }
+              >
+                {transactionDetails.type === 'income' ? 'Receita:' : 'Despesa:'}
+              </Label>
+
+              {transactionDetails.type === 'income' ? (
+                <BiTrendingUp size={25} color={color.colors.primary} />
+              ) : (
+                <BiTrendingDown size={25} color={color.colors.error} />
+              )}
+            </TextColor>
+          </Info>
+        </Content>
+      </Dialog>
+
+      {observationModalOpen && (
+        <Modal visible={observationModalOpen}>
           <div>
-            <IconWrapper>
-              <TbFileDescription size={25} />
-            </IconWrapper>
-            <Text>
-              <label>Título:</label>
-              <p>{transactionDetailsData?.title}</p>
-            </Text>
+            <h3>Observação</h3>
+            <p>{transactionDetails.observation}</p>
+            <button onClick={handleCloseObservationModal}>x</button>
           </div>
-
-          <div>
-            <IconWrapper>
-              <BsCalendar2Date size={20} />
-            </IconWrapper>
-            <Text>
-              <label>Data:</label>
-              <p>
-                {transactionDetailsData
-                  ? formatDate(transactionDetailsData.date)
-                  : ''}
-              </p>
-            </Text>
-          </div>
-
-          <div>
-            <IconWrapper>
-              <FaTags size={20} />
-            </IconWrapper>
-            <Text>
-              <label>Categoria:</label>
-              <p>{transactionDetailsData?.category.title}</p>
-            </Text>
-          </div>
-
-          <div>
-            <IconWrapper>
-              <FaMoneyBill size={20} />
-            </IconWrapper>
-            <Text>
-              <label>Valor:</label>
-              <p>
-                {transactionDetailsData
-                  ? formatCurrency(transactionDetailsData.amount)
-                  : ''}
-              </p>
-            </Text>
-          </div>
-
-          <div>
-            <IconWrapper>
-              <BiEditAlt size={20} />
-            </IconWrapper>
-            <Text>
-              <label>Observação:</label>
-              <p>{transactionDetailsData?.observation ?? 'N/A'}</p>
-            </Text>
-          </div>
-
-          <TextColor>
-            <Label
-              labelColor={
-                transactionDetailsData?.type === 'income'
-                  ? color.colors.primary
-                  : color.colors.error
-              }
-            >
-              {transactionDetailsData?.type === 'income'
-                ? 'Receita:'
-                : 'Despesa:'}
-            </Label>
-
-            {transactionDetailsData?.type === 'income' ? (
-              <BiTrendingUp size={25} color={color.colors.primary} />
-            ) : (
-              <BiTrendingDown size={25} color={color.colors.error} />
-            )}
-          </TextColor>
-        </Info>
-      </Content>
-    </Dialog>
+        </Modal>
+      )}
+    </>
   );
 }
--- src/Components/ModalTransactionsDetails/styles.ts
+++ src/Components/ModalTransactionsDetails/styles.ts
@@ -5,6 +5,10 @@ type LabelProps = {
   labelColor: string;
 };
 
+interface ModalProps {
+  visible: boolean; // Definição da propriedade visible
+}
+
 export const Content = styled.div`
   padding: 20px;
   h2 {
@@ -52,6 +56,24 @@ export const Text = styled.div`
     color: ${color.colors.neutral};
     font-size: 0.75rem;
   }
+
+  span  {
+    margin-top: 2px;
+    margin-bottom: 0;
+    color: ${color.colors.neutral};
+    font-size: 0.75rem;
+    white-space: nowrap;
+    max-width: 15ch;
+    height: 1.5rem;
+    text-align: center;
+    padding: 0.3rem;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    background-color: ${color.colors.black};
+    border-radius: 0.5rem;
+    border: solid 1px ${color.colors.white};
+border-style: dotted;
+  }
 `;
 
 export const IconWrapper = styled.div`
@@ -59,5 +81,52 @@ export const IconWrapper = styled.div`
   align-items: center;
   svg {
     color: ${color.colors.light};
+  }
+`;
+
+export const Modal = styled.div<ModalProps>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Cor de fundo com opacidade */
+  display: ${({ visible }) => (visible ? 'flex' : 'none')}; // Exibir ou ocultar o modal com base na propriedade visible
+  justify-content: center;
+  align-items: center;
+  z-index: 9999; /*index alto para garantir que o modal apareça acima de outros elementos */
+
+  div {
+    background-color: ${color.colors.white};
+    border-radius: 10px;
+    padding: 20px;
+    max-width: 80%;
+    max-height: 80%;
+    overflow-y: auto; /* Adiciona uma barra de rolagem vertical caso o conteúdo seja maior que a altura máxima */
+  }
+
+  h3 {
+    margin-bottom: 10px;
+    font-size: 1.2rem;
+    color: ${color.colors.dark}; /* Cor do texto */
+  }
+
+  p {
+    color: ${color.colors.dark}; /* Cor do texto */
+    margin-bottom: 20px;
+  }
+
+  button {
+    background-color: ${color.colors.primary}; /* Cor de fundo do botão */
+    color: ${color.colors.white}; /* Cor do texto do botão */
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    cursor: pointer;
+    font-size: 1rem;
+  }
+
+  button:hover {
+    background-color: ${color.colors.primaryDark}; /* Cor de fundo do botão ao passar o mouse */
   }
 `;
