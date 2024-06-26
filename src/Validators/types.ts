6794b09cab506130af29afeeb91e72ff370ab27c
uillasnr <wiliasreis@hotmail.com>
2024-06-25 21:03:42 -0300

    feat: add validation when creating user registration and logging in

--- src/Validators/types.ts
+++ src/Validators/types.ts
@@ -3,9 +3,13 @@ import {
   TransactionsFilterSchema,
   createCategorySchema,
   createTransactionSchema,
+  createUserSchema,
   finacialEvolutionFilterSchema,
+  userSchema,
 } from './schemas';
 
+export type UserData = z.infer<typeof userSchema>;
+export type CreateUser = z.infer<typeof createUserSchema>;
 export type CeateCategoryData = z.infer<typeof createCategorySchema>;
 export type CreateTransactionData = z.infer<typeof createTransactionSchema>;
 export type TransactionsFilterData = z.infer<typeof TransactionsFilterSchema>;
