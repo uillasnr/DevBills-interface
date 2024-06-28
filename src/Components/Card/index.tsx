fd963520e91190cec87baa77525e9e2f08412885
uillasnr <wiliasreis@hotmail.com>
2024-06-28 20:04:33 -0300

    feat: add card price

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,50 +1,28 @@
-import { useEffect, useState } from 'react';
-import {
-  Wallet,
-  TrendUp,
-  TrendDown,
-} from '@phosphor-icons/react';
+import { Wallet, TrendUp, TrendDown } from '@phosphor-icons/react';
 import { Container } from './styles';
 import { formatCurrency } from '../../utils/format-currency';
-import { api } from '../../services/Api';
 
 type CardProps = {
-  variant?: 'balance' | 'incomes' | 'expenses';
+  variant: 'balance' | 'incomes' | 'expenses';
   title: string;
   amount: number;
 };
 
 const iconMap = {
-  balance: <Wallet/>,
+  balance: <Wallet />,
   incomes: <TrendUp />,
   expenses: <TrendDown />,
 };
 
-export function Card({ variant = 'balance', title, amount }: CardProps) {
-  const [transactions, setTransactions] = useState<CardProps[]>([]);
-
-  useEffect(() => {
-    async function loadTransactions() {
-      try {
-        const response = await api.get('transactions/deshboard');
-        const { data } = response;
-        console.log(data);
-        setTransactions(data[variant]);
-      } catch (error) {
-        console.error('Error loading transactions:', error);
-      }
-    }
-
-    loadTransactions();
-  }, [variant]); 
-
+export function Card({ variant = "balance", title, amount }: CardProps) {
   return (
     <Container variant={variant}>
       <span>{title}</span>
-      <div> 
-      <strong>{formatCurrency(amount)}</strong>
-      {iconMap[variant]}
+      <div>
+        <strong>{formatCurrency(amount)}</strong>
+        {iconMap[variant]}
       </div>
     </Container>
   );
 }
+
